77312de9d28d39808c53b2b250f6ad4a3405bdb6
Michael Terrell <cwithmichael@gmail.com>
2024-12-27 11:20:31 -0600

    add google oauth

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
